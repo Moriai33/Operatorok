55555795ea1b0bd1a2f13a2762b40ea9e31e85bd
Szanyi István Moriai <szanyiistvan3@gmail.com>
2021-01-12 12:00:32 +0100

    Add project files.

---
+++ Operátorok/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operátorok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
